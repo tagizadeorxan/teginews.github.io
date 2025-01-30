2af5deca07f86fa6c4bd12cddefcb626313beaec
Orhan Taghizadeh <43483015+tagizadeorxan@users.noreply.github.com>
2025-01-30 17:40:28 +0400

    predictions

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const apiKey = '2c35ef06c56b4c26a0fe06229b0677df';
-const url = `https://newsapi.org/v2/top-headlines?category=technology&language=en&apiKey=${apiKey}`;
+const url = "./data.json";
 
 async function fetchNews() {
     try {
@@ -8,15 +7,23 @@ async function fetchNews() {
         
         let articles = data.articles;
         let newsContainer = document.getElementById("news-container");
-        
+
         articles.forEach(article => {
             let newsElement = document.createElement("div");
             newsElement.className = "article";
+            
+            // Format the date to a readable format
+            let publishedDate = new Date(article.publishedAt).toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "long",
+                day: "numeric"
+            });
+
             newsElement.innerHTML = `
                 <img src="${article.urlToImage}" alt="News Image">
                 <h2>${article.title}</h2>
                 <p>${article.description}</p>
-                <a href="${article.url}" target="_blank">Read more</a>
+                <p><strong>Published on:</strong> ${publishedDate}</p>
             `;
             newsContainer.appendChild(newsElement);
         });
